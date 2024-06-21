880b046ed15773a00e1919833fe2911a7530a54f
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-06-21 22:42:40 +0100

    feat: Handle webpage request

--- index.mjs
+++ index.mjs
@@ -1,10 +1,27 @@
 import { createServer } from "node:http";
+import { readFile } from "node:fs";
+import { URL } from "node:url";
 
 const host = "localhost";
 const port = 8080;
 
 const server = createServer((req, res) => {
-  const url = req.url;
+  const pathname = new URL(req.url, `http://${host}:${port}`).pathname;
+
+  readFile(
+    `.${pathname === "/" ? "/index.html" : pathname}`,
+    (err, content) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        readFile("./404.html", (err, content) => {
+          res.end(content);
+        });
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content);
+    }
+  );
 });
 
 server.listen(port, host, () => {
