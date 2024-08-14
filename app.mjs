87915b7e345f60041fad72603e3ebcf075a3e86f
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-08-14 14:46:06 +0100

    refactor: Rewrite server code using express

--- app.mjs
+++ app.mjs
@@ -1,28 +1,46 @@
-import { createServer } from "node:http";
+import express from "express";
 import { readFile } from "node:fs";
-import { URL } from "node:url";
 
+const app = express();
 const host = "localhost";
-const port = 8080;
+const port = 3000;
 
-const server = createServer((req, res) => {
-  const pathname = new URL(req.url, `http://${host}:${port}`).pathname;
-  const filePath = `.${pathname === "/" ? "/index" : pathname}.html`;
-
+app.get("/", (req, res) => {
   function showContent(err, content) {
     if (err) {
-      res.writeHead(404, { "Content-Type": "text/html" });
       return readFile("./404.html", (err, content) => {
-        res.end(content);
+        res.status(404).end(content);
       });
     }
-    res.writeHead(200, { "Content-Type": "text/html" });
     res.end(content);
   }
-
-  readFile(filePath, showContent);
+  readFile("index.html", showContent);
 });
 
-server.listen(port, host, () => {
+app.get("/about", (req, res) => {
+  function showContent(err, content) {
+    if (err) {
+      return readFile("./404.html", (err, content) => {
+        res.status(404).end(content);
+      });
+    }
+    res.end(content);
+  }
+  readFile("about.html", showContent);
+});
+
+app.get("/contact-me", (req, res) => {
+  function showContent(err, content) {
+    if (err) {
+      return readFile("./404.html", (err, content) => {
+        res.status(404).end(content);
+      });
+    }
+    res.end(content);
+  }
+  readFile("contact-me.html", showContent);
+});
+
+app.listen(port, () => {
   console.log(`Server running live at http://${host}:${port}`);
 });
