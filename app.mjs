e6d60f287a83aae84dab05f96701cc8e642abe8f
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-08-14 18:11:33 +0100

    refactor: Remove code block

--- app.mjs
+++ app.mjs
@@ -7,21 +7,14 @@ const host = "localhost";
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "index.html"));
-});
-
-app.get("/about", (req, res) => {
-  res.sendFile(join(__dirname, "about.html"));
-});
+app.get("/", (req, res) => res.sendFile(join(__dirname, "index.html")));
+app.get("/about", (req, res) => res.sendFile(join(__dirname, "about.html")));
 
 app.get("/contact-me", (req, res) => {
   res.sendFile(join(__dirname, "contact-me.html"));
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(join(__dirname, "404.html"));
-});
+app.get("*", (req, res) => res.sendFile(join(__dirname, "404.html")));
 
 app.listen(port, () => {
   console.log(`Server running live at http://${host}:${port}`);
